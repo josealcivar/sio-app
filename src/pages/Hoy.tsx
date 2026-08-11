a3cf5b23b569fb063ef0373ded8088887f5a5ee1
Jose Andre <joseandre@MacBook-Pro-de-Jose.local>
2026-08-11 14:35:43 -0500

    correcion errores card

--- src/pages/Hoy.tsx
+++ src/pages/Hoy.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { citasHoy } from "@/lib/datos"
-import { Card } from "@/components/ui/card"
+
 import { Button } from "@/components/ui/button"
 import { Link } from "react-router-dom"
 
