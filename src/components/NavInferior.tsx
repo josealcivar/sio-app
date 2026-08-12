62d07fa97ead59179d7bd38fa12d208ea4b84a0f
Jose Andre <joseandre@MacBook-Pro-de-Jose.local>
2026-08-12 01:25:07 -0500

    correcion boton menu inferior 2

--- src/components/NavInferior.tsx
+++ src/components/NavInferior.tsx
@@ -28,11 +28,11 @@ export default function NavInferior() {
          {({ isActive }) => (
             <>
               <div
-                className={`px-4 py-1 rounded-full transition-colors ${
+                className={`px-4 py-2 rounded-full transition-colors ${
                   isActive ? "bg-secondary" : ""
                 }`}
               >
-                <Icon size={22} />
+                <Icon size={25} />
               </div>
               {label}
             </>
